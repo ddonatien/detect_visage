b2206c47c68d08631b5d00ef11953efa18395691
Donatien Delehelle <donatien.delehelle@eyesnap.fr>
2018-07-23 11:33:39 +0200

    Ajout des fichiers

---
+++ app/detect.py
@@ -0,0 +1,40 @@
+"""Module contenant la classe DetectVisage"""
+import os
+import sys
+import cv2
+
+
+class DetectVisage:
+    """
+    Classe gerant le detecteur de visages
+    Utilise le mdele pre-entraine haarcascade_frontalface_default.xml
+    Methodes :
+       load_model
+       detect
+    """
+
+    def __init__(self):
+        self.filtre = ''
+        self.load_model()
+
+    def load_model(self):
+        """
+        Cette methode charge le modele de detection dans une variable de notre classe DetectVisage
+        """
+
+        root_path = os.path.dirname(sys.modules['__main__'].__file__)
+        model_path = os.path.join(root_path, 'models')
+        self.filtre = cv2.CascadeClassifier(
+            os.path.join(model_path, 'haarcascade_frontalface_default.xml'))
+
+    def detect(self, path_image):
+        """
+        Cette methode effectue la detection a partir du modele charge precedement
+        Parametre:
+          path_image : chemin vers l'image sur laquelle on souhaite faire une detection
+        """
+
+        image = cv2.imread(path_image)
+        image_niveaux_gris = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        face = self.filtre.detectMultiScale(image_niveaux_gris)
+        return False if len(face) == 0 else True
---
+++ run.py
@@ -0,0 +1,34 @@
+"""
+Interface console/detecteur
+Usage : python -m detect_visage.run -h
+"""
+import getopt
+import sys
+from detect_visage.app.detect import DetectVisage
+
+
+def usage():
+    print("python -m detect_visage.run -i <chemoon_vers_image>")
+    print("ou")
+    print("python -m detect_visage.run --image <chemin_vers_image>")
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'h:i:', ['help', 'image'])
+except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+
+chemin_image = ''
+for opt, arg in opts:
+    if opt in ('-h', '--help'):
+        usage()
+        sys.exit(1)
+    elif opt in ('-i', '--image'):
+        chemin_image = arg
+
+detecteur = DetectVisage()
+if detecteur.detect(chemin_image):
+    print("Visage detecte !")
+else:
+    print("Pas de visage")
